a2c140989e6b0b526ab0989b293ee5e5be759e7f
Renan Arques <renan.arques@gmail.com>
2024-06-08 12:09:52 -0300

    feat: login

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,85 @@
-import type { MetaFunction } from "@remix-run/node";
+import { json, MetaFunction } from "@remix-run/node"
+import { Link, useLoaderData, useNavigate } from "@remix-run/react"
+import { useState } from "react"
+
+import { Card, CardContent } from "~/components/ui/card"
+import { Label } from "~/components/ui/label"
+import { Input } from "~/components/ui/input"
+import { Button } from "~/components/ui/button"
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
+    { title: "BayMed" },
+    { description: "BayMed" }
+  ]
+}
+
+export const loader = async () => {
+  return json({ apiUrl: process.env.API_URL })
+}
 
 export default function Index() {
+  const { apiUrl } = useLoaderData<typeof loader>()
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const navigate = useNavigate()
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault()
+
+    const formData = new FormData()
+    formData.append("username", username)
+    formData.append("password", password)
+
+    const response = await fetch(`${apiUrl}/auth/login`, {
+      method: "POST",
+      body: formData,
+    })
+
+    if (response.ok) {
+      navigate("/dashboard")
+    } else {
+      console.error("Login failed")
+    }
+  }
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="flex min-h-screen bg-gray-100 justify-center items-center">
+      <Card className="w-full max-w-sm">
+        <CardContent className="flex flex-col items-center space-y-8">
+          <div className="flex flex-col items-center">
+            <img src="/icon.png" alt="BayMed Logo" className="h-20 w-20 mt-4" />
+            <h1 className="text-2xl font-bold">BayMed</h1>
+          </div>
+          <form onSubmit={handleSubmit} className="w-full space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="username">Apelido</Label>
+              <Input
+                id="username"
+                placeholder="Digite seu apelido"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="password">Senha</Label>
+              <Input
+                id="password"
+                placeholder="Digite sua senha"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="flex flex-col items-center pt-4 space-y-4">
+              <Button type="submit" className="w-full text-white">Entrar</Button>
+              <Link to="/register" className="text-primary hover:underline">
+                Criar uma conta
+              </Link>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
     </div>
-  );
+  )
 }
