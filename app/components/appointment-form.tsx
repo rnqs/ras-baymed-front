baa7a22f4141f5d20477c8383293a9b3e5472fa9
Renan Arques <renan.arques@gmail.com>
2024-06-13 15:50:51 -0300

    fix: add time to appointment

--- app/components/appointment-form.tsx
+++ app/components/appointment-form.tsx
@@ -21,7 +21,7 @@ export default function AppointmentForm() {
 
     try {
       const body = JSON.stringify({
-        dateTime: new Date().toISOString().split("T")[0],
+        dateTime: new Date().toISOString(),
         status: "PENDING",
         symptoms,
         patient,
